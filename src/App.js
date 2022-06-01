29a0b28478baac4acbb8829d8e2dd300f8afd695
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-06-01 23:22:40 +0200

    Add json-server + fetch Data in tablesRedux + App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTables } from './redux/tablesRedux';
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => fetchTables(dispatch), [dispatch]);
+
   return <div className='App'>Hello World!</div>;
 }
 
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -6,8 +6,17 @@ const createActionName = (actionName) => `app/tables/${actionName}`;
 const UPDATE_TABLES = createActionName('UPDATE_TABLES');
 
 // action creators
+export const updateTables = (payload) => ({ type: UPDATE_TABLES, payload });
+export const fetchTables = (dispatch) => {
+  fetch()
+    .then((res) => res.json())
+    .then((tables) => dispatch(updateTables(tables)));
+};
+
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
+    case UPDATE_TABLES:
+      return [...action.payload];
     default:
       return statePart;
   }

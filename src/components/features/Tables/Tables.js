f06c3595d280a95074a283b3c6b8a35e19f4e519
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-06-02 22:10:49 +0200

    Complete Tables Component

--- src/components/features/Tables/Tables.js
+++ src/components/features/Tables/Tables.js
@@ -1,25 +1,32 @@
-import { Row, ListGroup, Button } from 'react-bootstrap';
-import { Link, Navigate } from 'react-router-dom';
+import { Row, Col, ListGroup, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { getAllTables } from '../../../redux/tablesRedux';
 import { useSelector } from 'react-redux';
-import { capFirstLetter } from '../../../utils/capFirstLetter';
 
 const Tables = () => {
   const tablesList = useSelector((state) => getAllTables(state));
 
-  //   if (!tablesList) return <Navigate to='/' />;
   return (
     <Row className='justify-content-center'>
       <ListGroup className='my-4 p-0 col-md-8 align-self-center'>
         {tablesList.map((table) => (
           <ListGroup.Item
             key={table.id}
-            className='py-4 d-flex justify-content-between border-0 border-bottom'
+            className='py-4 d-flex align-items-center justify-content-between border-0 border-bottom'
           >
-            <h2 className='m-0'>Table {table.id}</h2>
-            <Link to={`/table/${table.id}`}>
-              <Button variant='primary'>Read more</Button>
-            </Link>
+            <Col className='col-lg-2'>
+              <h2 className='m-0'>Table {table.id}</h2>
+            </Col>
+            <Col>
+              <h5 className='m-0 fw-bold'>
+                Status: <span className='me-3 fw-normal'>{table.status}</span>
+              </h5>
+            </Col>
+            <Col className='d-flex justify-content-end'>
+              <Link to={`/table/${table.id}`}>
+                <Button variant='primary'>Show more</Button>
+              </Link>
+            </Col>
           </ListGroup.Item>
         ))}
       </ListGroup>

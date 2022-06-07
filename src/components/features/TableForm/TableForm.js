6ae0a605329bb488a8b4574acd1bb101ec9aa137
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-06-07 23:07:21 +0200

    Add TableFormPeople component

--- src/components/features/TableForm/TableForm.js
+++ src/components/features/TableForm/TableForm.js
@@ -1,10 +1,10 @@
-import styles from '../TableForm/TableForm.module.scss';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getAllTableStatuses } from '../../../redux/tableStatusesRedux';
 import { useForm } from 'react-hook-form';
 import TableFormStatuses from '../../views/TableFormStatuses/TableFormStatuses';
+import TableFormPeople from '../../views/TableFormPeople/TableFormPeople';
 
 const TableForm = ({ action, actionText, ...props }) => {
   const [tableStatus, setTableStatus] = useState(props.status || '');
@@ -49,62 +49,15 @@ const TableForm = ({ action, actionText, ...props }) => {
           allTableStatuses={allTableStatuses}
         />
 
-        <Form.Group
-          as={Row}
-          className='mb-4 align-items-center'
-          controlId='formTablePeople'
-        >
-          <Col className='m-0 col-sm-2 col-form-label'>
-            <Form.Label className='m-0 fw-bold'>People:</Form.Label>
-          </Col>
-          <Col className='m-0 pe-2 col-auto'>
-            <Form.Control
-              {...register('people', {
-                required: true,
-                min: 0,
-                max: 10,
-              })}
-              type='text'
-              value={people}
-              placeholder='0'
-              onChange={(e) => setPeople(e.target.value)}
-              className={styles.people}
-            />
-          </Col>
-          <Col className='m-0 p-0 col-auto'>
-            <Form.Text className='m-0'>/</Form.Text>
-          </Col>
-          <Col className='m-0 ps-2 col-auto'>
-            <Form.Control
-              {...register('maxPeople', {
-                required: true,
-                min: 0,
-                max: 10,
-              })}
-              type='text'
-              value={maxPeople}
-              placeholder='10'
-              onChange={(e) => setMaxPeople(e.target.value)}
-              className={styles.people}
-            />
-          </Col>
-          {peopleError && !errors.people && !errors.maxPeople && (
-            <small className='d-block form-text text-danger mt-2'>
-              Value has been updated. <br />
-              People amount cannot be lower than Max People amount.
-            </small>
-          )}
-          {errors.people && (
-            <small className='d-block form-text text-danger mt-2'>
-              People amount cannot be lower than 0 and greater than 10.
-            </small>
-          )}
-          {errors.maxPeople && (
-            <small className='d-block form-text text-danger mt-2'>
-              Max People amount cannot be lower than 0 and greater than 10.
-            </small>
-          )}
-        </Form.Group>
+        <TableFormPeople
+          register={register}
+          people={people}
+          setPeople={setPeople}
+          maxPeople={maxPeople}
+          setMaxPeople={setMaxPeople}
+          peopleError={peopleError}
+          errors={errors}
+        />
 
         {tableStatus === 'Busy' && (
           <Form.Group
@@ -124,7 +77,6 @@ const TableForm = ({ action, actionText, ...props }) => {
                 value={bill}
                 placeholder='0'
                 onChange={(e) => setBill(e.target.value)}
-                className={styles.input}
               />
             </Col>
           </Form.Group>
